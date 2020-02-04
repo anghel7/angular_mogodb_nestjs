2596c2753d573421dd31bd1b1a194dfdc5bc3f19
Angel Antezana Flores <ghehahanghel@gmail.com>
2020-02-03 20:45:19 -0400

    Asociando rutas a los componentes

--- frontend/src/app/curso/curso-routing.module.ts
+++ frontend/src/app/curso/curso-routing.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListaCursoComponent } from './components/lista-curso/lista-curso.component';
+import { EditarCursoComponent } from './components/editar-curso/editar-curso.component';
+import { VerCursoComponent } from './components/ver-curso/ver-curso.component';
+import { CrearCursoComponent } from './components/crear-curso/crear-curso.component';
 
 
 const routes: Routes = [
   {
     path: 'lista',
     component: ListaCursoComponent
+  },
+  {
+    path: 'editar',
+    component: EditarCursoComponent
+  },
+  {
+    path: 'ver',
+    component: VerCursoComponent
+  },
+  {
+    path: 'crear',
+    component: CrearCursoComponent
   }
 ];
 
