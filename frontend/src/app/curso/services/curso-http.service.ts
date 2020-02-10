13cd7640a1c198c51c342df5afa7d469c4ddeb9d
Angel Antezana Flores <ghehahanghel@gmail.com>
2020-02-10 19:45:44 -0400

    Creando observable para listar cursos

--- frontend/src/app/curso/services/curso-http.service.ts
+++ frontend/src/app/curso/services/curso-http.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Curso } from '../models';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,17 @@ export class CursoHttpService {
 
   test(): void {
     console.log('Testing CursohttpService');
+  }
+
+  getAllCursos(): Observable<Curso[]> {
+    let observableLista: Observable<Curso[]> = Observable.create(
+      (observer) => {
+        observer.next(this.listaCursos);
+        observer.complete();
+      }
+    );
+
+    return observableLista;
+
   }
 }
